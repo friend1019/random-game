bbbd4306535bcc0890d8acd81a21b1d64e4876ae
friend1019 <genie1019002@gmail.com>
2025-03-17 22:04:58 +0900

    으아

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import LotteryDraw from "./components/LotteryDraw";
+
+function App() {
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+      <LotteryDraw />
+    </div>
+  );
+}
+
+export default App;
